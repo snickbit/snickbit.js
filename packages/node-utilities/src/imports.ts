662adadc031f2902a0801523ee8106a78ab1b77f
Snickbit <snickbit@users.noreply.github.com>
2022-06-03 18:38:39 -0400

    fix: add generics to typings

--- packages/node-utilities/src/imports.ts
+++ packages/node-utilities/src/imports.ts
@@ -1,4 +1,4 @@
-import {arrayUnique, isObject} from '@snickbit/utilities'
+import {arrayUnique} from '@snickbit/utilities'
 
 /** @category Imports */
 export const isImport = (data: any) => typeof data === 'function' || data?.constructor.name === 'AsyncFunction' || Array.isArray(data)
@@ -6,37 +6,39 @@ export const isImport = (data: any) => typeof data === 'function' || data?.const
 /** @category Imports */
 export const isImportDefinition = (data: any) => data && data['run'] || data['handler'] || data['method']
 
-export type AnyFunction = (...args: any[]) => Promise<any> | any
+export type AnyFunction<Args = any, Results = any> = (...args: Args[]) => Promise<Results> | Results
 
-export type IObject = Record<string, any>
+export type ImportMethod<Args = any, Results = any> = (...args: Args[]) => Promise<Results> | Results
+
+export type IObject<T = any> = Record<string, T>
 
 /** @category Imports */
-export interface ImportDefinition {
-	default: AnyFunction | Array<any> | IObject
+export interface ImportDefinition<Args = any, Results = any> {
+	default: Array<ImportDefinition> | ImportDefinition<Args, Results> | ImportMethod<Args, Results>
 	name?: string
 	description?: string
-	describe?: string
+	aliases?: string[]
 
 	[key: string]: any
 
-	(...args: any[]): any
+	(...args: Args[]): Promise<Results> | Results
 }
 
 /** @category Imports */
-export type ImportRecords = Record<string, AnyFunction | ImportDefinition>
+export type ImportRecords<Args = any, Results = any> = Record<string, ImportDefinition | ImportMethod<Args, Results>>
 
 /** @category Imports */
-export type RecordOfImportRecords = Record<string, ImportRecords>
+export type RecordOfImportRecords<Args = any, Results = any> = Record<string, ImportRecords<Args, Results>>
 
 /** @category Imports */
-export interface ParsedImport {
+export interface ParsedImport<Args = any, Results = any> {
 	name: string
 	aliases: string[]
 	description?: string
-	handler: AnyFunction
+	handler: ImportMethod<Args, Results>
 }
 
-export type ParsedImportRecords = Record<string, ParsedImport>
+export type ParsedImportRecords<Args = any, Results = any> = Record<string, ParsedImport<Args, Results>>
 
 export interface UnparsedImport {
 	name?: string
@@ -44,17 +46,17 @@ export interface UnparsedImport {
 	alias?: string
 	description?: string
 	describe?: string
-	handler?: AnyFunction
-	method?: AnyFunction
-	default?: AnyFunction
-	run?: AnyFunction
+	handler?: ImportMethod
+	method?: ImportMethod
+	default?: ImportMethod
+	run?: ImportMethod
 }
 
 /**
  * Parse imports from `import * as name from 'path'` statements into a more manageable format.
  * @category Imports
  */
-export function parseImports(imports: ImportRecords | RecordOfImportRecords, parent?: string): ParsedImportRecords {
+export function parseImports<Args = any, Results = any>(imports: ImportRecords<Args, Results> | RecordOfImportRecords<Args, Results>, parent?: string): ParsedImportRecords<Args, Results> {
 	const importRecords = {}
 	for (const [importItem, data] of Object.entries(imports)) {
 		let parent_name = parent ? parent : ''
